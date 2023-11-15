99d0f4f771fa0d5e7578ea86777d9e912eb2a489
YgorHenriqueLima <118612433+YgorHenriqueLima@users.noreply.github.com>
2023-11-15 10:12:28 -0300

    adicionando o projeto biblioteca java

---
+++ src/main/java/entity/Aluno.java
@@ -0,0 +1,49 @@
+package entity;
+public class Aluno {
+    private String nome;
+    private Curso curso;
+    private int matricula;
+    private String email;
+    private String fone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+    
+}
---
+++ src/main/java/entity/Livro.java
@@ -0,0 +1,58 @@
+package entity;
+public class Livro {
+    private String titulo;
+    private int quantidade;
+    private Estante estante;
+    private Prateleira prateleira;
+    private Autor autor;
+    private String isbn;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Estante getEstante() {
+        return estante;
+    }
+
+    public void setEstante(Estante estante) {
+        this.estante = estante;
+    }
+
+    public Prateleira getPrateleira() {
+        return prateleira;
+    }
+
+    public void setPrateleira(Prateleira prateleira) {
+        this.prateleira = prateleira;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+}
